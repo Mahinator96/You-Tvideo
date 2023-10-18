f399ddc846b79cbbc48b3a2473e7ee9cd1c63ee8
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-10-18 19:59:07 +0300

    Finish day 2

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//   const playerControl = document.querySelector('.video__player iframe');
+//   // const playerDoc = playerControl.querySelector('#document')
+//   const playerDoc = playerControl.contentDocument
+//   console.log('playerControl: ', playerControl);
+//   console.log('playerDoc: ', playerDoc);
+// })
